57588eed9f72f53539221aa370f4dfade6624bbb
Sébastien Leduc [sebastien@sleduc.fr] <Sbastien Leduc [sebastien@sleduc.fr]>
2012-03-07 18:40:45 +0100

    using regexp syntax with chuck module

--- modules/chuck/__init__.py
+++ modules/chuck/__init__.py
@@ -5,7 +5,7 @@ import urllib
 import lib.utils
 import re
 from BeautifulSoup import BeautifulSoup
-from lib.modules import SyncModule, defaultcmd
+from lib.modules import SyncModule, answercmd
 
 class CmdChuck(SyncModule):
     def __init__(self, bot):
@@ -16,18 +16,25 @@ chuck [n] : Affiche le fact [n]"""
                             bot,  
                             desc = desc,
                             command = "chuck",
+                            lock_time = 2,
                             )
 
-    @defaultcmd
+    @answercmd(r"(?P<index>\d+)$")
+    def answer_int(self, sender, message):
+        """!chuck [n]"""
+        index = message.group("index")
+        page = 'http://www.chucknorrisfacts.fr/index.php?p=detail_fact&fact=%s' % (index)
+        return CmdChuck.get_chuck(page)
+
+    @answercmd(r"^$")
     def answer(self, sender, message):
-        if (not message.strip()):
-            url = urllib.urlopen('http://www.chucknorrisfacts.fr/index.php?p=parcourir&tri=aleatoire')
-            byid = False
-        elif message.isdigit():
-            url = urllib.urlopen('http://www.chucknorrisfacts.fr/index.php?p=detail_fact&fact=%s' % (message))
-            byid = True
-        else:
-            return "Utilise un entier si tu veux une quote spécifique, ou rien si tu préfères aller à Toire"
+        """!chuck"""
+        page = 'http://www.chucknorrisfacts.fr/index.php?p=parcourir&tri=aleatoire'
+        return CmdChuck.get_chuck(page)
+
+    @staticmethod
+    def get_chuck(url):
+        url = urllib.urlopen(url)
         contenu = url.read()
         soup = BeautifulSoup(contenu)
         fact = soup.findAll("div", {"class": "fact"})[0]
