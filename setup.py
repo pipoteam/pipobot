72f06aae052990a31e245cc0a08e2e80c6195e44
Antoine Pierlot-Garcin <antoine@bokbox.com>
2012-05-06 18:47:34 +0200

    Install modules in /usr/share/pipobot/modules.

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 from setuptools import setup, find_packages
+import os
 
 from pipobot import __version__
+
+def list_all_data_files(dst_path, src_path):
+    f = []
+    for (dirpath, dirname, filenames) in os.walk(src_path):
+        f.append((os.path.join(dst_path, dirpath),
+                  [os.path.join(dirpath, filename) for filename in filenames]))
+    return f
 
 setup(
     name = 'pipobot',
@@ -11,7 +19,7 @@ setup(
 #   url = '',
 
     packages = find_packages(exclude=["modules*"]),
-#   data_files = [],
+    data_files = list_all_data_files('/usr/share/pipobot', 'modules'),
 #   scripts = [],
 
     entry_points = {
