494208ad068e5dfc7bda7c6b2750c035d69c5352
Vincent Duvert <vincent@duvert.net>
2012-07-08 17:55:57 +0200

    Changes to setup.py: 	- New translation process, handled by the babel module. 	- Translations moved to a subfolder of the pipobot module so they are automatically installed with the module in all cases 	- Added documentation on the internationalisation/translation process 	- Better version handling (setup.py should not import any pipobot modules)

--- translation/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#Compiling .po file to generate .mo translation file used by gettext
-language=$1
-if [ $# = 0 ];then
-    language=fr_FR
-fi
-msgfmt -o $language.mo $language.po
-mkdir -p ../pipobot/locale/$language/LC_MESSAGES
-mv $language.mo ../pipobot/locale/$language/LC_MESSAGES/pipobot.mo
