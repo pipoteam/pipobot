776dac0f2a4b0bf88e24b14f3d53f50829cd2827
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-11-05 15:39:29 +0100

    info_modules: presentation

--- pipobot/info_modules.py
+++ pipobot/info_modules.py
@@ -40,29 +40,43 @@ def is_in_module(path):
 def info_modules(bot, module_paths):
     for module_path in module_paths:
         chdir(module_path)
-        pipobot_modules = [path for path in Path('.').iterdir() if path.is_dir() and (path / '__init__.py').is_file()]
+        pipobot_modules = sorted(path for path in Path('.').iterdir() if path.is_dir() and (path / '__init__.py').is_file())
 
-        for path in pipobot_modules:
-            print('#',  path)
+        with open('info_modules.md', 'w') as md:
+            for path in pipobot_modules:
+                print('#',  path, file=md)
 
-            externals = set()
-            for root, filename in [(r, f) for r, _, files in walk(str(path)) for f in files if f.endswith('.py')]:
-                with Path(root, filename).open() as f:
-                    mod = set(next(i for i in m.groups() if i is not None).split('.')[0] for l in f for m in IMPORTS.finditer(l))
-                    externals |= set(m for m in mod if not is_in_module(join(root, m)) and is_external(m))
+                externals = set()
+                for root, filename in [(r, f) for r, _, files in walk(str(path)) for f in files if f.endswith('.py')]:
+                    with Path(root, filename).open() as f:
+                        mod = set(next(i for i in m.groups() if i is not None).split('.')[0] for l in f for m in IMPORTS.finditer(l))
+                        externals |= set(m for m in mod if not is_in_module(join(root, m)) and is_external(m))
 
-            if externals:
-                print('## depends:')
-                for external in externals:
-                    print('*', external)
+                if externals:
+                    print('depends:', file=md)
+                    for external in externals:
+                        print('*', external, file=md)
+                    print(file=md)
 
-            try:
-                module = import_module(str(path))
-                for name, member in getmembers(module, isclass):
-                    if hasattr(member, 'mro') and member.mro()[1].__name__ in MODULE_TYPES and name not in MODULE_TYPES:
-                        print('##', name)
-                        pipobot_module = member(bot)
-                        print(pipobot_module.desc)
-            except Exception as e:
-                print(':exclamation: import failed: %s' % e.message)
-            print()
+                try:
+                    module = import_module(str(path))
+                    for name, member in getmembers(module, isclass):
+                        if hasattr(member, 'mro') and member.mro()[1].__name__ in MODULE_TYPES and name not in MODULE_TYPES:
+                            print('%s:' % name, file=md)
+                            print(file=md)
+                            desc = member(bot).desc
+                            if isinstance(desc, dict):
+                                if '' in desc:
+                                    print(desc[''], file=md)
+                                    print(file=md)
+                                print('|Command|Description|', file=md)
+                                print('|-------|-----------|', file=md)
+                                for key, value in desc.items():
+                                    if key:
+                                        print('|%s|%s|' % (key, value), file=md)
+                                print(file=md)
+                            else:
+                                print('```\n%s\n```\n' % desc, file=md)
+                except Exception as e:
+                    print(':exclamation: import failed: %s' % e.message, file=md)
+                print(file=md)
